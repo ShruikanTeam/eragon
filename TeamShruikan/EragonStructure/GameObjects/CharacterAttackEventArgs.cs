6803721843d438fe53e83f658dcb06aa9d2cd19e
vgendov <vegeta_m@abv.bg>
2014-10-09 18:53:04 +0300

    Added User Input Interface

---
+++ TeamShruikan/EragonStructure/GameObjects/CharacterAttackEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EragonStructure.GameObjects
+{
+    public class CharacterAttackEventArgs : EventArgs
+    {
+        public int AttackX { get; set; }
+        public int AttackY { get; set; }
+
+        public CharacterAttackEventArgs(int x, int y)
+        {
+            this.AttackX = x;
+            this.AttackY = y;
+        }
+    }
+}
---
+++ TeamShruikan/EragonStructure/Interfaces/KeyboardController.cs
@@ -0,0 +1,60 @@
+﻿using EragonStructure.GameObjects;
+using System;
+using System.Windows.Forms;
+
+namespace EragonStructure.Interfaces
+{
+    public class KeyboardController : IUserInput
+    {
+        public event EventHandler OnRightPressed;
+        public event EventHandler OnLeftPressed;
+        public event EventHandler OnUpPressed;
+        public event EventHandler OnDownPressed;
+        public event EventHandler OnSpacePressed;
+
+        public KeyboardController(Form form)
+        {
+            form.KeyDown += FormKeyDown;
+        }
+
+        private void FormKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    if (this.OnUpPressed != null)
+                    {
+                        this.OnUpPressed(this, new EventArgs());
+                    }
+                    break;
+                case Keys.Right:
+                    if (this.OnRightPressed != null)
+                    {
+                        this.OnRightPressed(this, new EventArgs());
+                    }
+                    break;
+                case Keys.Down:
+                    if (this.OnDownPressed != null)
+                    {
+                        this.OnDownPressed(this, new EventArgs());
+                    }
+                    break;
+                case Keys.Left:
+                    if (this.OnLeftPressed != null)
+                    {
+                        this.OnLeftPressed(this, new EventArgs());
+                    }
+                    break;
+                case Keys.Space:
+                    if (this.OnSpacePressed != null)
+                    {
+                        this.OnSpacePressed(this, new CharacterAttackEventArgs(e.X, e.Y));
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}

a0a19f9652f9b1af2013cb56000707fdad9d09bd
Yavor Mitev <yavor2000@yahoo.com>
2014-10-17 16:23:13 +0300

    added GameLauncher.cs

---
+++ TeamShruikan/EragonStructure/GameLauncher.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using EragonStructure.UI;
+
+namespace EragonStructure
+{
+    public class GameLauncher
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Window());
+        }
+    }
+}

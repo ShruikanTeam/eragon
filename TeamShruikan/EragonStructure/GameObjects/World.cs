381673bb84b7fc4779727e96ae523890c27b0bdd
Yavor Mitev <yavor2000@yahoo.com>
2014-10-19 14:57:07 +0300

    fixed missing World.cs

---
+++ TeamShruikan/EragonStructure/GameObjects/World.cs
@@ -0,0 +1,41 @@
+﻿namespace EragonStructure.GameObjects
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+
+    public class World : IDrawable
+    {
+        private Bitmap canvas;
+        private IList<GameObject> objects;
+
+        public World(int width, int height, Picture background)
+        {
+                canvas = new Bitmap(background.Image, width, height);
+        }
+
+        internal void AddObject(GameObject gameObject)
+        {
+            if (objects == null)
+            {
+                objects = new List<GameObject>();
+            }
+
+            objects.Add(gameObject);
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawImage(canvas, 0f, 0f);
+
+            if (objects != null && objects.Count > 0)
+            {
+                foreach (var gameObject in objects)
+                {
+                    gameObject.Draw(g);
+                }
+            }
+        }
+    }
+}

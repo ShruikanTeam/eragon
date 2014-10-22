ce512cb9d76442e756b80007bee868ca73f9e1a5
tarlit <tarlitan@gmail.com>
2014-10-23 02:14:16 +0300

    Last Version :) Login and Choice

---
+++ EragonStructure/EragonStructure/UI/HeroChoice.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EragonStructure.UI
+{
+    public partial class HeroChoice : Form
+    {
+        public HeroChoice()
+        {
+            InitializeComponent();
+        }
+
+        private void pctIceMage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctWitch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctIceWitch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctViking_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctRedKnight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctNinja_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctArcher_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            //Window form = new Window();
+            //form.Show();
+
+            //this.Hide();
+        }
+    }
+}

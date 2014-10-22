fdb05ab3e667ad5cd32ccb35c2ced3382f92fa60
Yavor Mitev <yavor2000@yahoo.com>
2014-10-23 00:05:45 +0300

    shooter fixed

--- TeamShruikan/EragonStructure/GameObjects/Shooter.cs
+++ TeamShruikan/EragonStructure/GameObjects/Shooter.cs
@@ -5,8 +5,8 @@
     public class Shooter : Player
     {
         public Shooter(Point point, Size size, Picture picture, string name, int attack, int defence, 
-            int range, int currentHealthPoints, int maxHealthPoints, int movementsSpeed, int currentLevel, int experienceNeeded)
-            : base(point, size, picture, name, attack, defence, range, currentHealthPoints, maxHealthPoints, movementsSpeed, currentLevel, experienceNeeded)
+            int range, int currentHealthPoints, int maxHealthPoints, int movementsSpeed, int currentLevel, int experienceNeeded, bool imgDir)
+            : base(point, size, picture, name, attack, defence, range, currentHealthPoints, maxHealthPoints, movementsSpeed, currentLevel, experienceNeeded, imgDir)
         {
             this.Attack += 5;
             this.Defense -= 10;

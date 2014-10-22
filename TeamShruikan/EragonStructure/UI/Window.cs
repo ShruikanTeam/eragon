b679189329205f39a4765567cd2030f947cd3e3e
Yavor Mitev <yavor2000@yahoo.com>
2014-10-22 17:14:38 +0300

    added game engine

--- TeamShruikan/EragonStructure/UI/Window.cs
+++ TeamShruikan/EragonStructure/UI/Window.cs
@@ -11,7 +11,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using EragonStructure.Enumerations;
+using EragonStructure.GameEngin;
 using EragonStructure.GameObjects;
+using EragonStructure.Interfaces;
 using EragonStructure.Structs;
 using Point = System.Drawing.Point;
 
@@ -22,8 +24,12 @@ namespace EragonStructure.UI
         [DllImport("user32")]
         static extern short GetAsyncKeyState(Keys vKey);
 
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(System.Int32 vKey);
+
         public const int TimeInterval = 30;
-        private Bitmap backBuffer;
+        //private Bitmap backBuffer;
+        private Painter painter;
         private Picture backGround;
         private Picture heroPicture;
         private Picture creepPicture;
@@ -37,59 +43,23 @@ namespace EragonStructure.UI
 
             SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.DoubleBuffer, true);
 
-            Load += Form1_CreateBackBuffer;
             Paint += Form1_Paint;
-
-            LoadImages();
-            world = new World(ClientSize.Width, ClientSize.Height, backGround);
-            hero = new Fighter(new Structs.Point(100, 350),
-                new EragonStructure.Structs.Size(100, 100),
-                heroPicture,
-                "bai ivan",
-                90, 120, 10, 100, 200, 10, 1, 10);
-            creep = new Creep(new Structs.Point(400, 150),
-                new EragonStructure.Structs.Size(100, 100),
-                creepPicture,
-                "creep",
-                90, 120, 10, 60, 100, 3);
+            //world = new World(ClientSize.Width, ClientSize.Height, backGround);
             
-            world.AddObject(creep);
-            world.AddObject(hero);
-
+            KeyboardController controller = new KeyboardController(this);
+            painter = new Painter(this);
+            Engine engine = new Engine(controller, painter);
             var windowTimer = new Timer() { Interval = TimeInterval };
-            windowTimer.Tick += (s, args) => UpdateScreen();
+            windowTimer.Tick += (s, args) => engine.Play();
             windowTimer.Start();
-        }
-
-        public void LoadImages()
-        {
-            string resPath = Directory.GetCurrentDirectory() + @"..\..\..\Resources\";
-            try
-            {
-                backGround = new Picture(new Bitmap(Image.FromFile(resPath + @"Backgrounds\Eragon_Saphira.jpg"), new System.Drawing.Size(ClientSize.Width, ClientSize.Height)));
-                heroPicture = new Picture(new Bitmap(Image.FromFile(resPath + @"NiceGuys\archer2.png"), new System.Drawing.Size(80, 100)));
-                creepPicture = new Picture(new Bitmap(Image.FromFile(resPath + @"BadGuys\BossIceMonster01.png"), new System.Drawing.Size(100, 100)));
-            }
-            catch (IOException)
-            {
-                Console.WriteLine("cannot read from file!");
-            }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            if (backBuffer != null)
+            if (painter.Canvas != null)
             {
-                e.Graphics.DrawImageUnscaled(backBuffer, Point.Empty);
+                e.Graphics.DrawImageUnscaled(painter.Canvas, Point.Empty);
             }
-        }
-
-        private void Form1_CreateBackBuffer(object sender, EventArgs e)
-        {
-            if (backBuffer != null)
-                backBuffer.Dispose();
-
-            backBuffer = new Bitmap(ClientSize.Width, ClientSize.Height);
         }
 
         private void CheckKeyInput()
@@ -149,19 +119,19 @@ namespace EragonStructure.UI
             //}
         }
 
-        private void UpdateScreen()
+        public void UpdateScreen()
         {
             CheckKeyInput();
 
-            if (backBuffer != null)
-            {
-                using (var g = Graphics.FromImage(backBuffer))
-                {
-                    world.Draw(g);
-                }
-
-                Invalidate();
-            }
+            //if (backBuffer != null)
+            //{
+            //    using (var g = Graphics.FromImage(backBuffer))
+            //    {
+            //        world.Draw(g);
+            //    }
+                
+            //    Invalidate();
+            //}
         }
     }
 }

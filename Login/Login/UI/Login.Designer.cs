c3dc34865874b1910dfaa47a05a4e696da920fea
tarlit <tarlitan@gmail.com>
2014-10-23 01:40:11 +0300

    Login and Choice Forms

---
+++ Login/Login/UI/Login.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace EragonStructure.UI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbWelcome = new System.Windows.Forms.Label();
+            this.lbUserName = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbWelcome
+            // 
+            this.lbWelcome.AutoSize = true;
+            this.lbWelcome.BackColor = System.Drawing.Color.Transparent;
+            this.lbWelcome.Font = new System.Drawing.Font("Algerian", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbWelcome.Image = global::EragonStructure.Properties.Resources.btnGoldBig;
+            this.lbWelcome.Location = new System.Drawing.Point(430, 41);
+            this.lbWelcome.Name = "lbWelcome";
+            this.lbWelcome.Padding = new System.Windows.Forms.Padding(82, 22, 82, 22);
+            this.lbWelcome.Size = new System.Drawing.Size(431, 79);
+            this.lbWelcome.TabIndex = 0;
+            this.lbWelcome.Text = " Game \"Eragon\"";
+            // 
+            // lbUserName
+            // 
+            this.lbUserName.AutoSize = true;
+            this.lbUserName.BackColor = System.Drawing.Color.Transparent;
+            this.lbUserName.Font = new System.Drawing.Font("Algerian", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUserName.Image = global::EragonStructure.Properties.Resources.btnGoldSmall;
+            this.lbUserName.Location = new System.Drawing.Point(126, 226);
+            this.lbUserName.Name = "lbUserName";
+            this.lbUserName.Padding = new System.Windows.Forms.Padding(12);
+            this.lbUserName.Size = new System.Drawing.Size(137, 45);
+            this.lbUserName.TabIndex = 1;
+            this.lbUserName.Text = "User Name";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.BackColor = System.Drawing.Color.Transparent;
+            this.lbPassword.Image = global::EragonStructure.Properties.Resources.btnGoldSmall;
+            this.lbPassword.Location = new System.Drawing.Point(307, 226);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Padding = new System.Windows.Forms.Padding(12);
+            this.lbPassword.Size = new System.Drawing.Size(131, 45);
+            this.lbPassword.TabIndex = 2;
+            this.lbPassword.Text = "Password";
+            // 
+            // tbUserName
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.SlateGray;
+            this.textBox1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.textBox1.Location = new System.Drawing.Point(125, 287);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Name = "tbUserName";
+            this.textBox1.Size = new System.Drawing.Size(138, 33);
+            this.textBox1.TabIndex = 3;
+            // 
+            // tbPassword
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.SlateGray;
+            this.textBox2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.textBox2.Location = new System.Drawing.Point(307, 287);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox2.Name = "tbPassword";
+            this.textBox2.Size = new System.Drawing.Size(131, 33);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.SlateBlue;
+            this.btnLogin.BackgroundImage = global::EragonStructure.Properties.Resources.btnGoldSmall;
+            this.btnLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLogin.Location = new System.Drawing.Point(222, 344);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(124, 53);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::EragonStructure.Properties.Resources.CastleInClouds;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1260, 760);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUserName);
+            this.Controls.Add(this.lbWelcome);
+            this.Font = new System.Drawing.Font("Algerian", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbWelcome;
+        private System.Windows.Forms.Label lbUserName;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
